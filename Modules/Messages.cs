b43ebb81fce5d950ce072f367e93a5981d0581c3
fxhxyz4 <fxhxyz8@gmail.com>
2025-01-11 11:15:38 +0200

    update messages class

--- Modules/Messages.cs
+++ Modules/Messages.cs
@@ -1,65 +1,74 @@
+using System;
+
 namespace TwT
 {
     public class Messages
     {
-        /*
-         * Error
-         *
-         * @params {string msg}
-         */
+       /*
+       * @public
+       *
+       * @params {string msg}
+       */
         public static void Error(string msg)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"\n\n[ERROR] {msg}\n\n");
-
-            Console.ForegroundColor = ConsoleColor.White;
+            WriteMessage($"[ERROR] {msg}", ConsoleColor.Red);
         }
 
         /*
-         * Info
+         * @public
          *
          * @params {string msg}
          */
         public static void Info(string msg)
         {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine($"[INFO] {msg}");
-
-            Console.ForegroundColor = ConsoleColor.White;
+            WriteMessage($"[INFO] {msg}", ConsoleColor.Blue);
         }
 
         /*
-         * Debug
+         * @public
          *
          * @params {string msg}
          */
         public static void Debug(string msg)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"\n\n[DEBUG] {msg}\n\n");
-
-            Console.ForegroundColor = ConsoleColor.White;
+            WriteMessage($"[DEBUG] {msg}", ConsoleColor.Green);
         }
 
         /*
-         * Log
+         * @public
          *
          * @params {string msg}
          */
         public static void Log(string msg)
         {
-            Console.WriteLine($"[LOG] {msg}");
+            WriteMessage(msg, ConsoleColor.White);
         }
 
         /*
-         * Warn
+         * @public
          *
          * @params {string msg}
          */
         public static void Warn(string msg)
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($"\n[WARN] {msg}\n");
+            WriteMessage($"[WARN] {msg}", ConsoleColor.Yellow);
+        }
+
+        /*
+         * @private
+         *
+         * @params {string msg}
+         * @params {ConsoleColor color}
+         */
+        private static void WriteMessage(string msg, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine($"{msg}");
+
+            if (Settings.WriteLogs == true)
+            {
+              Logs.WriteSync(msg);
+            }
 
             Console.ForegroundColor = ConsoleColor.White;
         }
