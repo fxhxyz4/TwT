a713a4d9def633e2d93e68360dbf1a947012c5f2
fxhxyz <fxhxyz7@proton.me>
2025-01-16 10:33:25 +0000

    update cli

--- Modules/CLI.cs
+++ Modules/CLI.cs
@@ -1,48 +1,36 @@
 using System;
+using System.Collections.Generic;
 
 namespace TwT
 {
   public class CLI
   {
-    public void Help()
+    private readonly Dictionary<string, ICommand> _commands;
+
+    public CLI()
     {
-      Messages.Log("Available Commands:");
-      Messages.Log("help - Display this help information.");
-      Messages.Log("open - Open log file\nUse: open {path to log file}");
+      _commands = new Dictionary<string, ICommand>
+      {
+        { "help", new HelpCommand() },
+        { "disconnect", new DisconnectCommand() },
+        { "clear", new ClearCommand() },
+        { "echo", new  EchoCommand() },
+        { "open", new OpenCommand() },
+        { "pyramid", new PyramidCommand() },
+        { "ban", new BanCommand() },
+      };
     }
 
     public void ExecuteCommand(string cmd, string[] args)
     {
-      switch (cmd.ToLower())
+      if (_commands.TryGetValue(cmd.ToLower(), out var command))
       {
-        case "help":
-          Help();
-          break;
-        case "open":
-          if (args.Length > 0)
-          {
-            string path = args[0];
-
-            if (File.Exists(path))
-            {
-              Logs.ReadSync(path);
-            }
-            else
-            {
-              Messages.Error($"The log file at '{path}' does not exist.");
-            }
-          }
-          else
-          {
-            Messages.Error("Missing path to the log file. Use: open {path to log file}");
-          }
-          break;
-
-        default:
-          Messages.Error($"Unknown command: '{cmd}'. Type 'help' for a list of available commands.");
-          break;
+        command.Execute(args);
+      }
+      else
+      {
+        Messages.Error($"Unknown command: '{cmd}'. Type 'help' for a list of available commands.");
       }
     }
   }
 }
-
